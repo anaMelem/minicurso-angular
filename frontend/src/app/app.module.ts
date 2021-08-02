cb8ac15283499e92aa9ddd576fcb425a4e0b60c4
Ana <anamelem4@gmail.com>
2021-08-02 16:16:10 -0300

    Nav Component created

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,20 +6,28 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/templates/header/header.component';
 
-import{ MatToolbarModule} from '@angular/material/toolbar';
-import { FooterComponent } from './components/templates/footer/footer.component'
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { FooterComponent } from './components/templates/footer/footer.component';
+import { NavComponent } from './components/templates/nav/nav.component'
+
+import { MatSidenavModule}from '@angular/material/sidenav'
+import {MatListModule }from '@angular/material/list'
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
